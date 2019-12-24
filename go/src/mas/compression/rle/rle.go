fab364362dc01ea3130f6eb55bedc0ab8c1c9c7a
Thomas Kim <kimthomas109@gmail.com>
2019-12-23 19:39:54 -0800

    add rle impl

--- go/src/mas/compression/rle/rle.go
+++ go/src/mas/compression/rle/rle.go
@@ -2,27 +2,49 @@ package rle
 
 import (
 	"strconv"
+	"strings"
+	"unicode"
 )
 
 func Encode(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
 	ret := ""
 	prev := ""
 	count := 0
+
 	for _, curr := range s {
 		if string(curr) != prev && count != 0 {
 			ret += strconv.Itoa(count) + prev
-			count = 0
+			count = 1
 		} else {
-			prev = string(curr)
 			count++
 		}
+		prev = string(curr)
 	}
-	if count != 0 {
-		ret += strconv.Itoa(count) + prev
-	}
+
+	ret += strconv.Itoa(count) + prev
 	return ret
 }
 
 func Decode(s string) string {
-	return ""
+	ret := ""
+	cs := strings.FieldsFunc(s, func(c rune) bool {
+		return unicode.IsNumber(c)
+	})
+
+	nums := strings.FieldsFunc(s, func(c rune) bool {
+		return !unicode.IsNumber(c)
+	})
+
+	for i, c := range cs {
+		n, err := strconv.Atoi(nums[i])
+		if err != nil {
+			return ""
+		}
+		ret += strings.Repeat(string(c), n)
+	}
+	return ret
 }
--- go/src/mas/compression/rle/rle_test.go
+++ go/src/mas/compression/rle/rle_test.go
@@ -6,11 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var tests map[string]string = map[string]string{
+	"aaaaa":                     "5a",
+	"a":                         "1a",
+	"aabbc":                     "2a2b1c",
+	"asdfasdf":                  "1a1s1d1f1a1s1d1f",
+	"uuuhhuuu":                  "3u2h3u",
+	"###$$%":                    "3#2$1%",
+	"uuuuuuuuuuuiiiiiiiiiiiooo": "11u11i3o",
+}
+
 func TestEncode(t *testing.T) {
-	tests := map[string]string{
-		"aaaaa": "5a",
-	}
 	for input, expected := range tests {
 		assert.Equal(t, expected, Encode(input))
+	}
+}
+
+func TestDecode(t *testing.T) {
+	for expected, input := range tests {
+		assert.Equal(t, expected, Decode(input))
 	}
 }
